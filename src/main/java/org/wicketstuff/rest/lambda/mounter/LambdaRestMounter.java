cd0737e384546143ba14fde3ad82c9067b0d7e21
Andrea Del Bene <adelbene@apache.org>
2016-12-12 17:34:46 +0100

    More JavaDoc

--- src/main/java/org/wicketstuff/rest/lambda/mounter/LambdaRestMounter.java
+++ src/main/java/org/wicketstuff/rest/lambda/mounter/LambdaRestMounter.java
@@ -39,101 +39,238 @@ import org.wicketstuff.rest.utils.wicket.AttributesWrapper;
  */
 public class LambdaRestMounter
 {
+	
+	/** The application. */
 	private final WebApplication application;
 	
+	/**
+	 * Constructor for the mounter.
+	 * 
+	 * @param application
+	 * 			The web application we are using.
+	 */
 	public LambdaRestMounter(WebApplication application) 
 	{
 		this.application = application;
 	}
 
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method POST.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void post(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.POST, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method POST.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void post(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer) 
 	{		
 		mountRestResource(HttpMethod.POST, path, respondConsumer);
 	}
 	
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method GET.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void get(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.GET, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method GET.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void get(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer) 
 	{		
 		mountRestResource(HttpMethod.GET, path, respondConsumer);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method PUT.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void put(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
 		mountRestResource(HttpMethod.PUT, path, respondConsumer);
 	}
 	
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method PUT.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void put(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.PUT, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method DELETE.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void delete(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
 		mountRestResource(HttpMethod.DELETE, path, respondConsumer);
 	}
 	
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method DELETE.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void delete(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.DELETE, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method OPTIONS.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void options(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
 		mountRestResource(HttpMethod.OPTIONS, path, respondConsumer);
 	}
 	
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method OPTIONS.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void options(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.OPTIONS, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method PATCH.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void patch(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
 		mountRestResource(HttpMethod.PATCH, path, respondConsumer);
 	}
 
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method PATCH.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void patch(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.PATCH, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method HEAD.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void head(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
 		mountRestResource(HttpMethod.HEAD, path, respondConsumer);
 	}
 
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method HEAD.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void head(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.HEAD, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for 
+	 * HTTP method TRACE.
+	 *
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 */
 	public void trace(final String path, final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
 		mountRestResource(HttpMethod.TRACE, path, respondConsumer);
 	}
 
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for 
+	 * HTTP method TRACE.
+	 *
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 */
 	public void trace(final String path, final SerializableFunction<AttributesWrapper, Object> respondFunction,
 			final SerializableFunction<Object, String> outputFunction) 
 	{		
 		mountRestResource(HttpMethod.TRACE, path, respondFunction, outputFunction);
 	}
 	
+	/**
+	 * Mount a {@link TextOutputLambdaResource} to the given path and for the given http method
+	 *
+	 * @param httpMethod the http method
+	 * @param path the mounting path
+	 * @param respondFunction the respond function
+	 * @param outputFunction the output function
+	 * @return the resource mapper
+	 */
 	public ResourceMapper mountRestResource(final HttpMethod httpMethod, final String path, 
 											final SerializableFunction<AttributesWrapper, Object> respondFunction,
 											final SerializableFunction<Object, String> outputFunction)
@@ -142,6 +279,14 @@ public class LambdaRestMounter
 		return mountRestResource(httpMethod, path, resource);
 	}
 
+	/**
+	 * Mount a {@link SimpleLambdaResource} to the given path and for the given http method
+	 *
+	 * @param httpMethod the http method
+	 * @param path the mounting path
+	 * @param respondConsumer the respond consumer
+	 * @return the resource mapper
+	 */
 	public ResourceMapper mountRestResource(final HttpMethod httpMethod, final String path, 
 			final SerializableConsumer<AttributesWrapper> respondConsumer)
 	{
@@ -149,6 +294,14 @@ public class LambdaRestMounter
 		return mountRestResource(httpMethod, path, resource);
 	}
 
+	/**
+	 * Mount rest resource.
+	 *
+	 * @param httpMethod the http method
+	 * @param path the mounting path
+	 * @param resource the resource
+	 * @return the resource mapper
+	 */
 	protected ResourceMapper mountRestResource(final HttpMethod httpMethod, final String path,
 			IResource resource) 
 	{
